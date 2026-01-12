9e45ef92ac4b53c158ddad52436e78d93dc73272
MehulArya <mehularya007@gmail.com>
2026-01-12 21:01:52 +0000

    Updating the admin.py for left_panel and center_panel

--- Quiet-Sprout/QuietSprout/center_panel/admin.py
+++ Quiet-Sprout/QuietSprout/center_panel/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+import center_panel.models as c_models
 # Register your models here.
+
+admin.site.register(c_models.MidnightLogs)
--- Quiet-Sprout/QuietSprout/left_panel/admin.py
+++ Quiet-Sprout/QuietSprout/left_panel/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+import left_panel.models as l_models
 # Register your models here.
+
+admin.site.register(l_models.Identity)
